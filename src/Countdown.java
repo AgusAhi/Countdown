e37952033f341a74796591290c628597721b488a
aad <aguahidol@alu.edu.gva.es>
2024-01-15 19:44:20 +0100

    a

---
+++ src/Countdown.java
@@ -0,0 +1,17 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Countdown {
+    public static void main(String[] args) throws InterruptedException {
+        Integer time = Integer.parseInt(args[0]);
+        Queue<Integer> queue = new LinkedList<Integer>();
+        for (int i = time; i >= 0; i--) {
+            queue.add(i);
+        }
+
+        while(!queue.isEmpty()) {
+            System.out.println(queue.remove());
+            Thread.sleep(1000);
+        }
+    }
+}
